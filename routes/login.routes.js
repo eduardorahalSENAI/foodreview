fb513e01a5db5b859969503af48cb1d8962c4814
eduardorahalsenai <eduardo.barbosa@edu.sc.senai.br>
2022-08-22 21:52:05 -0300

    autenticação pronta no login

--- routes/login.routes.js
+++ routes/login.routes.js
@@ -1,7 +1,50 @@
 import express from 'express';
+import User from '../models/User.js';
+import bcrypt from 'bcrypt';
+import Jwt from 'jsonwebtoken';
 
 const login = express.Router();
 
-login.get('/', (req, res) => res.send("Rota de Login"));
+login.post('/', async (req, res) => {
+    const { email, password } = req.body;
+
+    // 1º - Pesquisar a existência deste usuário no BD
+
+    const registeredUser = await User.findOne({ where: { email } }).catch(
+        (err) => {
+            console.log("Error: ", err);
+        }
+    );
+
+    // 2º - Caso inexistente, informar usuário
+
+    if(!registeredUser)
+        return res
+            .status(400)
+            .json({ message: "Email ou Senha incorretos." });
+    
+    // 3º - Validar a senha
+    // 3.1 - Caso senha inválida, informar usuário
+
+    if(!bcrypt.compareSync(password, registeredUser.password))
+        return res
+            .status(400)
+            .json({ message: "Email ou Senha incorretos." });
+
+    // 3.2 - Caso senha correta, gerar token
+    
+    const token = Jwt.sign(
+        { 
+            id: registeredUser.id, 
+            email: registeredUser.email 
+        },
+        process.env.JWT_SECRET,
+        { expiresIn: '1h' }
+    );
+
+    // 3.3 - Enviar Token ao Usuário
+
+    res.json({ message: "Bem-vindo!", token: token });
+});
 
 export default login;
