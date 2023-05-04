a9a1fc8be391d69bc5d3164d73d8029e8a6ad58d
Professor Senai <Professor@SN-391607.edu.fiesc.com.br>
2023-05-04 19:19:14 -0300

    Aula do Dia 03052023

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import User from "../models/User.js";
 
 const user = express.Router();
 
@@ -6,6 +7,35 @@ user.get('/', function(req, res) {
     res.send("Rota de Usuário")
 });
 
-user.get('/register', (req, res) => res.send("Cadastro de Novo Usuário"));
+user.post('/register', async (req, res) => {
+    const { name, email, password, admin } = req.body;
+
+    const alreadyExistsUser = await User.findOne(
+        { where: { email } }
+    ).catch((err) => console.log("Error: ", err));
+
+    if (alreadyExistsUser) {
+        console.log("Usuário Existente: " + alreadyExistsUser);
+        return res
+            .status(409)
+            .json({ message: "E-mail já utilizado por outro usuário." })
+    }
+
+    const newUser = new User({name, email, password, admin});
+                    
+    const savedUser = await newUser.save().catch((err) => {
+                        console.log("Error: ", err);
+                        res
+                            .status(500)
+                            .json({error: "Não foi posível cadastrar o usuário."})
+    });
+
+    if (savedUser){
+        console.log(savedUser);
+        res.json({message: "Obrigado pelo Cadastro!"})
+    }
+
+    }
+);
 
 export default user;
